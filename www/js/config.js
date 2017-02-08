2e38aa499d7a8f8574165b1340d5974616d1882e
kaushita <kaushita123@gmail.com>
2017-02-09 00:01:44 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 angular.module('your_app_name.config', [])
-.constant('WORDPRESS_API_URL', 'http://wordpress.startapplabs.com/blog/api/')
+.constant('WORDPRESS_API_URL', 'http://www.dekhbe.com/wp-json/wp/v2//')
 .constant('WORDPRESS_PUSH_URL', 'http://wordpress.startapplabs.com/blog/push/')
 .constant('GCM_SENDER_ID', '574597432927')
 
